2ca442e878bf9b8ee6d05a45a02d2d86dd74eefa
LeeYeoNyeong <yn4103@naver.com>
2025-06-13 14:59:40 +0900

    Final assignment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,6 +7,27 @@ const config: HardhatUserConfig = {
   vyper: {
     version: "0.3.10",
   },
+  networks: {
+    kairos: {
+      url: "https://public-en-kairos.node.kaia.io",
+      accounts: ["0x908678d4389b94f6160b4ae5c7749002ad25d9d832bbd6c1db94a9519d105e70"],
+    }
+  },  
+  etherscan: {
+    apiKey: {
+      kairos: "unnecessary",
+    },
+    customChains: [
+      {
+        network: "kairos",
+        chainId: 1001,
+        urls: {
+          apiURL: "https://kairos-api.kaiascan.io/hardhat-verify",
+          browserURL: "https://kairos.kaiascan.io",
+        }
+      },
+    ]
+  }
 };
 
 export default config;
